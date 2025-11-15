9b69e46f735c768f78eaee7f30839ee8b6f55490
Nairi-Khachatryan <nairi.khachatryan357@gmail.com>
2025-11-15 10:55:56 +0400

    conect Mongo-Db add mongo link in .env file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,27 @@
+// import { avatarRouter } from './routes/avatar.routes.js';
+// import { blockRouter } from './routes/block.routes.js';
+// import { authRouter } from './routes/auth.routes.js';
+// import { userRouter } from './routes/user.routes.js';
+// import { toDoRouter } from './routes/toDo.routes.js';
+import { connectDb } from './config/db';
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
-const PORT = 5051;
+const PORT = process.env.PORT || 5052;
 
-app.get('/', (_req, res) => {
-  res.send('Backend is running');
+app.use(cors());
+app.use(express.json());
+// app.use('/auth', authRouter);
+// app.use('/user', userRouter);
+// app.use('/toDo', toDoRouter);
+// app.use('/block', blockRouter);
+// app.use('/avatar', avatarRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at Port ${PORT}`);
+  connectDb();
 });
-
-app.listen(PORT, () => console.log(`Backend listening on port ${PORT}`));
